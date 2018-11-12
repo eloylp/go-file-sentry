79df0930b58dbc2ffe9325c03e26c0abb33afe2e
eloylp <eloylpdev@gmail.com>
2018-11-12 18:42:02 +0100

    Adding data read capabilities to file.

---
+++ file/_test/file_test.go
@@ -0,0 +1,18 @@
+package _test
+
+import (
+	"github.com/eloylp/go-file-sentry/file"
+	"path/filepath"
+	"testing"
+)
+
+func TestFileGetData(t *testing.T) {
+	f := file.File{}
+	f.Path = filepath.Join("file.txt")
+	data := f.GetData()
+	text := string(data)
+	expectedContent := "This is a test file."
+	if text != expectedContent {
+		t.Errorf("Expected content of file is %s , result was %s", expectedContent, text)
+	}
+}
--- file/file.go
+++ file/file.go
@@ -1,10 +1,23 @@
 package file
 
-import . "time"
+import (
+	"io/ioutil"
+	"log"
+	. "time"
+)
 
 type File struct {
 	Path string
 	FQDN string
 	Sum  string
 	Time Time
+}
+
+func (file File) GetData() []byte {
+
+	dat, err := ioutil.ReadFile(file.Path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return dat
 }
