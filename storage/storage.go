f0ddef55febcbacecf2bab927af3fdbb86269cbb
eloylp <eloylpdev@gmail.com>
2018-12-16 19:31:21 +0100

    Will refactor storage module. Better code readability. Splitting code in multiple files.

--- storage/storage.go
+++ storage/storage.go
@@ -1,8 +1,6 @@
 package storage
 
 import (
-	"crypto/md5"
-	"encoding/hex"
 	"github.com/eloylp/go-file-sentry/file"
 	"github.com/eloylp/go-file-sentry/scan"
 	"io/ioutil"
@@ -14,38 +12,6 @@ import (
 )
 
 const diffExtension string = ".diff"
-
-type StorageUnit struct {
-	DiffContent []byte
-	File        file.File
-}
-
-func (unit *StorageUnit) GetDiffContent() []byte {
-	return unit.DiffContent
-}
-
-func (unit *StorageUnit) GetFileFQDN() string {
-	return unit.File.FQDN
-}
-
-func (unit *StorageUnit) GetFileName() string {
-	return unit.File.GetName()
-}
-
-func (unit *StorageUnit) GetFileData() []byte {
-	return unit.File.GetData()
-}
-
-func (unit *StorageUnit) GetFilePath() string {
-	return unit.File.Path
-}
-
-func (unit *StorageUnit) CalculateName() string {
-	hasher := md5.New()
-	hasher.Write([]byte(unit.GetFilePath()))
-	containerName := hex.EncodeToString(hasher.Sum(nil))
-	return containerName
-}
 
 func AddNewEntry(rootPath string, storageUnitContent StorageUnit) {
 	destination := filepath.Join(rootPath, storageUnitContent.CalculateName(), storageUnitContent.GetFileFQDN())
@@ -80,9 +46,9 @@ func FindLatestVersion(rootPath string, scannedFile file.File) (storageUnit Stor
 	storedVersions, err := ioutil.ReadDir(fileContainerDirAbsolutePath)
 	failIfError(err)
 
-	lastFile := calculateLastFileReference(storedVersions)
+	lastVersion := calculateLastVersionReference(storedVersions)
 
-	fullFilePath := filepath.Join(fileContainerDirAbsolutePath, lastFile.Name(), scannedFile.GetName())
+	fullFilePath := filepath.Join(fileContainerDirAbsolutePath, lastVersion.Name(), scannedFile.GetName())
 	requestedFile := scan.ScanFile(fullFilePath)
 	diffContent, err := ioutil.ReadFile(fullFilePath + diffExtension)
 	failIfError(err)
@@ -93,12 +59,12 @@ func FindLatestVersion(rootPath string, scannedFile file.File) (storageUnit Stor
 	return storageUnit, err
 }
 
-func calculateLastFileReference(storedVersions []os.FileInfo) os.FileInfo {
+func calculateLastVersionReference(storedVersions []os.FileInfo) os.FileInfo {
 	const containerPartsSeparator string = "-"
 	const containerTimePartLayout = "20060102150405"
 
 	var lastTime time.Time
-	var lastFile os.FileInfo
+	var lastVersion os.FileInfo
 	for _, storedVersion := range storedVersions {
 		entryName := storedVersion.Name()
 		parts := strings.Split(entryName, containerPartsSeparator)
@@ -107,8 +73,8 @@ func calculateLastFileReference(storedVersions []os.FileInfo) os.FileInfo {
 		failIfError(err)
 		if parsedTime.After(lastTime) {
 			lastTime = parsedTime
-			lastFile = storedVersion
+			lastVersion = storedVersion
 		}
 	}
-	return lastFile
+	return lastVersion
 }
---
+++ storage/unit.go
@@ -0,0 +1,39 @@
+package storage
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"github.com/eloylp/go-file-sentry/file"
+)
+
+type StorageUnit struct {
+	DiffContent []byte
+	File        file.File
+}
+
+func (unit *StorageUnit) GetDiffContent() []byte {
+	return unit.DiffContent
+}
+
+func (unit *StorageUnit) GetFileFQDN() string {
+	return unit.File.FQDN
+}
+
+func (unit *StorageUnit) GetFileName() string {
+	return unit.File.GetName()
+}
+
+func (unit *StorageUnit) GetFileData() []byte {
+	return unit.File.GetData()
+}
+
+func (unit *StorageUnit) GetFilePath() string {
+	return unit.File.Path
+}
+
+func (unit *StorageUnit) CalculateName() string {
+	hasher := md5.New()
+	hasher.Write([]byte(unit.GetFilePath()))
+	containerName := hex.EncodeToString(hasher.Sum(nil))
+	return containerName
+}
