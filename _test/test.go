a8feb98ba75df2ff604ee2ca66ddc2c9e472f05e
eloylp <eloylpdev@gmail.com>
2018-12-23 11:58:01 +0100

    Moving common test logic to its own package.

---
+++ _test/test.go
@@ -0,0 +1,69 @@
+package _test
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"github.com/nu7hatch/gouuid"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func WriteFileToTestFolder(testFolderPath string, name string, content string) string {
+	filePath := filepath.Join(testFolderPath, string(os.PathSeparator), name)
+	testFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0666)
+	FailIfError(err)
+	defer testFile.Close()
+	_, err = testFile.WriteString(content)
+	FailIfError(err)
+	return filePath
+}
+func FailIfError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+const testRootFolderName string = "tmp"
+const testFolderPrefix string = "go_file_sentry_test_"
+
+func CreateTestStorageFolder() string {
+	uuidGen, _ := uuid.NewV4()
+	testFolder := filepath.Join(string(os.PathSeparator), testRootFolderName, testFolderPrefix+uuidGen.String())
+	_ = os.Mkdir(testFolder, 0755)
+	return testFolder
+}
+
+func CreateFixedTestStorageFolder(suffix string) string {
+	testFolder := filepath.Join(string(os.PathSeparator), testRootFolderName, testFolderPrefix+suffix)
+	_ = os.Mkdir(testFolder, 0755)
+	return testFolder
+}
+
+func CleanTestStorageFolder(path string) {
+	err := os.RemoveAll(path)
+	FailIfError(err)
+}
+
+func CalculateMd5(input string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(input))
+	containerName := hex.EncodeToString(hasher.Sum(nil))
+	return containerName
+}
+
+func FsExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
+}
+
+func GetTestResource(resourceName string) string {
+	const testResourceDir = "test"
+	return filepath.Join(testResourceDir, resourceName)
+}
--- storage/storage_test.go
+++ storage/storage_test.go
@@ -1,82 +1,20 @@
 package storage_test
 
 import (
-	"crypto/md5"
-	"encoding/hex"
+	"github.com/eloylp/go-file-sentry/_test"
 	"github.com/eloylp/go-file-sentry/file"
 	"github.com/eloylp/go-file-sentry/storage"
-	"github.com/nu7hatch/gouuid"
 	"io/ioutil"
-	"log"
 	"os"
 	"path/filepath"
 	"testing"
 )
 
-func writeFileToTestFolder(testFolderPath string, name string, content string) string {
-	filePath := filepath.Join(testFolderPath, string(os.PathSeparator), name)
-	testFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0666)
-	failIfError(err)
-	defer testFile.Close()
-	_, err = testFile.WriteString(content)
-	failIfError(err)
-	return filePath
-}
-func failIfError(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-const testRootFolderName string = "tmp"
-const testFolderPrefix string = "go_file_sentry_test_"
-
-func createTestStorageFolder() string {
-	uuidGen, _ := uuid.NewV4()
-	testFolder := filepath.Join(string(os.PathSeparator), testRootFolderName, testFolderPrefix+uuidGen.String())
-	_ = os.Mkdir(testFolder, 0755)
-	return testFolder
-}
-
-func createFixedTestStorageFolder(suffix string) string {
-	testFolder := filepath.Join(string(os.PathSeparator), testRootFolderName, testFolderPrefix+suffix)
-	_ = os.Mkdir(testFolder, 0755)
-	return testFolder
-}
-
-func cleanTestStorageFolder(path string) {
-	err := os.RemoveAll(path)
-	failIfError(err)
-}
-
-func calculateMd5(input string) string {
-	hasher := md5.New()
-	hasher.Write([]byte(input))
-	containerName := hex.EncodeToString(hasher.Sum(nil))
-	return containerName
-}
-
-func fsExists(path string) (bool, error) {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true, nil
-	}
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return true, err
-}
-
-func getTestResource(resourceName string) string {
-	const testResourceDir = "test"
-	return filepath.Join(testResourceDir, resourceName)
-}
-
 func TestAddNewEntry(t *testing.T) {
 
-	testFolderPath := createTestStorageFolder()
-	defer cleanTestStorageFolder(testFolderPath)
-	testFilePath := writeFileToTestFolder(testFolderPath, "test.txt", "Content")
+	testFolderPath := _test.CreateTestStorageFolder()
+	defer _test.CleanTestStorageFolder(testFolderPath)
+	testFilePath := _test.WriteFileToTestFolder(testFolderPath, "test.txt", "Content")
 	sampleFile := file.NewFile(testFilePath)
 	storageUnit := storage.NewStorageUnit([]byte{}, sampleFile)
 	storage.AddNewEntry(testFolderPath, storageUnit)
@@ -85,7 +23,7 @@ func TestAddNewEntry(t *testing.T) {
 		storageUnit.CalculateName(),
 		sampleFile.GetFQDN(),
 	)
-	exist, err := fsExists(expectedFolderPath)
+	exist, err := _test.FsExists(expectedFolderPath)
 
 	if err != nil {
 		t.Errorf(err.Error())
@@ -97,41 +35,41 @@ func TestAddNewEntry(t *testing.T) {
 
 func TestAddEntryContent(t *testing.T) {
 
-	testFolderPath := createTestStorageFolder()
-	defer cleanTestStorageFolder(testFolderPath)
+	testFolderPath := _test.CreateTestStorageFolder()
+	defer _test.CleanTestStorageFolder(testFolderPath)
 	testFileName := "fileA"
 	testFileContent := "Content A	"
-	filePath := writeFileToTestFolder(testFolderPath, testFileName, testFileContent)
+	filePath := _test.WriteFileToTestFolder(testFolderPath, testFileName, testFileContent)
 
 	sampleFile := file.NewFile(filePath)
 	sampleFileDiffContent := []byte("Differential patch")
 	storageUnit := storage.NewStorageUnit(sampleFileDiffContent, sampleFile)
-	containerName := calculateMd5(filePath)
+	containerName := _test.CalculateMd5(filePath)
 	containerFolder := filepath.Join(
 		testFolderPath,
 		containerName,
 		sampleFile.GetFQDN())
 
 	err := os.MkdirAll(containerFolder, 0755)
-	failIfError(err)
+	_test.FailIfError(err)
 
 	storage.AddEntryContent(testFolderPath, storageUnit)
 	expectedFilePath := filepath.Join(containerFolder, testFileName)
 
-	exist, _ := fsExists(expectedFilePath)
+	exist, _ := _test.FsExists(expectedFilePath)
 	if !exist {
 		t.Fatal("file doesnt exist in storage engine.")
 	}
 
 	expectedFileDiffPath := expectedFilePath + ".diff"
-	exist, err = fsExists(expectedFileDiffPath)
-	failIfError(err)
+	exist, err = _test.FsExists(expectedFileDiffPath)
+	_test.FailIfError(err)
 	if !exist {
 		t.Fatal("file diff doesnt exist in storage engine.")
 	}
 
 	diffContent, err := ioutil.ReadFile(expectedFileDiffPath)
-	failIfError(err)
+	_test.FailIfError(err)
 	if string(diffContent) != string(sampleFileDiffContent) {
 		t.Fatal("Unexpected file diff content.")
 	}
@@ -139,13 +77,13 @@ func TestAddEntryContent(t *testing.T) {
 
 func TestFindLatestVersion(t *testing.T) {
 
-	testFolderPath := createFixedTestStorageFolder("TestFindLatestVersion")
-	defer cleanTestStorageFolder(testFolderPath)
-	samplePath := writeFileToTestFolder(testFolderPath, "fstab", "Content C")
+	testFolderPath := _test.CreateFixedTestStorageFolder("TestFindLatestVersion")
+	defer _test.CleanTestStorageFolder(testFolderPath)
+	samplePath := _test.WriteFileToTestFolder(testFolderPath, "fstab", "Content C")
 	sampleFile := file.NewFile(samplePath)
-	rootPath := getTestResource("root_sample")
+	rootPath := _test.GetTestResource("root_sample")
 	recoveredFile, err := storage.FindLatestVersion(rootPath, sampleFile)
-	failIfError(err)
+	_test.FailIfError(err)
 
 	if "Content C" != string(recoveredFile.GetFileData()) {
 		t.Fatal("Retrieved file is not the latest.")
