35ac265afdbbb54f5c95cd9ad6042dc7a1a21bb9
eloylp <eloylpdev@gmail.com>
2018-12-31 18:42:51 +0100

    fixing type error.

--- config/config.go
+++ config/config.go
@@ -1,22 +1,22 @@
 package config
 
-type Version struct {
+type Config struct {
 	storagePath  string
 	watchedFiles []string
 }
 
-func (v *Version) WatchedFiles() []string {
+func (v *Config) WatchedFiles() []string {
 	return v.watchedFiles
 }
 
-func NewVersion(storagePath string, watchedFiles []string) *Version {
-	return &Version{storagePath: storagePath, watchedFiles: watchedFiles}
+func NewVersion(storagePath string, watchedFiles []string) *Config {
+	return &Config{storagePath: storagePath, watchedFiles: watchedFiles}
 }
 
-func (v *Version) StoragePath() string {
+func (v *Config) StoragePath() string {
 	return v.storagePath
 }
 
-func (v *Version) SetStoragePath(storagePath string) {
+func (v *Config) SetStoragePath(storagePath string) {
 	v.storagePath = storagePath
 }
