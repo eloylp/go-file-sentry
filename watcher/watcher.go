17ba89fec140243ef1cc5fb99889bc4fdc3f8f9b
eloylp <eloylpdev@gmail.com>
2019-01-28 19:43:03 +0100

    improvements in code at watcher pkg. Subscribing to more than one fs event and logging ignored events.

--- watcher/watcher.go
+++ watcher/watcher.go
@@ -6,18 +6,36 @@ import (
 	"log"
 )
 
+var wEvents = []notify.Event{
+	notify.InCloseWrite,
+	notify.InModify,
+	notify.Write,
+	notify.Remove,
+}
+
 func WFile(wFile *file.File, handler func(file *file.File)) {
-	events := make(chan notify.EventInfo, 1)
-	err := notify.Watch(wFile.Path(), events, notify.InCloseWrite)
+	events := make(chan notify.EventInfo)
+	err := notify.Watch(wFile.Path(), events, notify.All)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("Starting watching file %s", wFile.Path())
 	for event := range events {
-		if event.Event() == notify.InCloseWrite {
+		if isWEvent(event) {
 			log.Printf("Changes watched in %s, handling a new version ...", wFile.Path())
 			wFile.LoadMetadata()
 			handler(wFile)
+		} else {
+			log.Printf("Ignoring event %s for file %s", event.Event(), wFile.Path())
 		}
 	}
+}
+
+func isWEvent(event notify.EventInfo) bool {
+	for _, wEvent := range wEvents {
+		if event.Event() == wEvent {
+			return true
+		}
+	}
+	return false
 }
